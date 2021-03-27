483b97d932450cac4058f4a79b5874b41212140c
Your Name <azulazen@adolph_12@hotmail.com.au>
2021-03-27 20:32:43 +0800

    Initial backend created

---
+++ server/routes/api/posts.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongdb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts 
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+
+});
+
+// Add Posts 
+
+router.post('/', async(req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+
+    res.status(201).send();
+});
+
+
+// Update Posts 
+
+
+//Delete Posts 
+router.delete('/:id', async (req, res) =>{
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongdb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection(){
+    const client = await mongdb.MongoClient.connect
+    ("mongodb+srv://abc123:asher@journal.9uxae.mongodb.net/projectNotes?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true
+    })
+
+    return client.db('projectNotes').collection('posts');
+}
+
+
+
+
+module.exports = router;
