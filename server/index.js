2f2332c7477689efd6e35e3b81b6c7e6f6fb45c0
Your Name <azulazen@adolph_12@hotmail.com.au>
2021-04-10 02:00:03 +0800

    priorities

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
 const app = express();
 
 //Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = require('./routes/api/posts');
 
+const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
+
 
 //Handle Production
 if(process.env.NODE_ENV === 'production'){
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -16,7 +16,8 @@ router.post('/', async(req, res) => {
     const posts = await loadPostsCollection();
     await posts.insertOne({
         text: req.body.text,
-        createdAt: new Date()
+        createdAt: new Date(),
+        priority: req.body.priority
     });
 
     res.status(201).send();
